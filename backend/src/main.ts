fde61c4eab119f0decfb227eaf405948175c8421
Mikhail-Shcheglov <112893458+Mikhail-Shcheglov@users.noreply.github.com>
2024-06-12 03:12:08 +0300

    feat: update cors origin list

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://mikhail-shcheglov.nomoredomainswork.ru'
+  });
   await app.listen(3000);
 }
 bootstrap();
